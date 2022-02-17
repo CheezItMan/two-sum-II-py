3ad8963e682e7c63b7673fb94cdee90c29ca2d2d
Chris McAnally <chris@adadevelopersacademy.org>
2022-02-17 13:19:15 -0800

    added comments

--- two_sum/two_sum.py
+++ two_sum/two_sum.py
@@ -2,7 +2,9 @@ from typing import List
 
 
 def two_sum(nums: List[int], target: int) -> List[int]:
+    # Find the 1st index in the list
     for i in range(len(nums)):
+        # find the other index starting at i+1
         for j in range(i+1, len(nums)):
             if nums[i] + nums[j] == target:
                 return [i, j]
