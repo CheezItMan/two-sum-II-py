8b492a0f47e6456508df8e8c314c0177dc69eac1
Chris McAnally <chris@adadevelopersacademy.org>
2022-02-17 12:08:47 -0800

    initial tests

---
+++ tests/two_sum_test.py
@@ -0,0 +1,29 @@
+import pytest
+from lib.two_sum import two_sum
+
+
+def test_example_one():
+    nums = [2, 7, 11, 15]
+    target = 9
+
+    assert two_sum(nums, target) == [0, 1]
+
+
+def test_example_two():
+    nums = [2, 3, 4]
+    target = 6
+
+    assert two_sum(nums, target) == [0, 2]
+
+
+def test_example_three():
+    nums = [-1, 0]
+    target = -1
+
+    assert two_sum(nums, target) == [0, 1]
+
+
+@pytest.skip
+def test_edge_case():
+    # You write an edge-case test here
+    pass
